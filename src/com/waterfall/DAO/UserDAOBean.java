d47bfd6b8429a8e019d2872d990ab43f14a2f6df
Lena <lena.stridsberg@gmail.com>
2016-09-12 13:43:41 +0200

    changing .ignore so it works for mac

---
+++ src/com/waterfall/DAO/UserDAOBean.java
@@ -0,0 +1,26 @@
+package com.waterfall.DAO;
+
+import javax.ejb.Stateful;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import com.waterfall.models.User;
+
+
+
+@Stateful
+public class UserDAOBean {
+	
+	@PersistenceContext
+	private EntityManager em;
+	
+	public boolean saveUser(User user) {
+		
+		if(em.merge(user) != null){
+			return true;
+		}
+		return false;
+	}
+
+}
---
+++ src/com/waterfall/backingbeans/UserBean.java
@@ -0,0 +1,154 @@
+package com.waterfall.backingbeans;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.ejb.EJB;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+import com.waterfall.EJB.interfaces.LocalUser;
+import com.waterfall.models.User;
+
+import sun.reflect.generics.tree.VoidDescriptor;
+
+@Named(value = "userBean")
+@SessionScoped
+public class UserBean implements Serializable{
+	
+	private static final long serialVersionUID = 3773988104720989698L;
+	private String firstName;
+	private String lastName;
+	private String username;
+	private String city;
+	private String dob;
+	private String email;
+	private String gender;
+	private String password;
+	
+	@EJB 
+	private LocalUser userEJB;
+	
+	public String registerNewUser(){
+		User user = new User();
+		user.setFirstname(firstName);
+		user.setLastname(lastName);
+		user.setUsername(username);
+		user.setEmail(email);
+		user.setCity(city);
+		user.setGender(gender);
+		user.setPassword(password);
+		
+		// temporary date for database purposes
+		Date exampleDate = new Date(0,0,0);
+		exampleDate.setDate(14);
+		exampleDate.setYear(1964);
+		exampleDate.setMonth(04);
+		
+		user.setBirthdate(exampleDate);
+		
+		userEJB.saveUser(user);
+		
+//		System.out.println(user);
+//		System.out.println("I BEAN!");
+		
+		
+		return "index";
+	}
+	
+	
+	
+	public String getPassword() {
+		return password;
+	}
+
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+
+
+	public String getLastName() {
+		return lastName;
+	}
+
+
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+
+
+	public String getUsername() {
+		return username;
+	}
+
+
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+
+
+	public String getCity() {
+		return city;
+	}
+
+
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+
+
+	public String getDob() {
+		return dob;
+	}
+
+
+
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+
+	public String getGender() {
+		return gender;
+	}
+
+
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+}
