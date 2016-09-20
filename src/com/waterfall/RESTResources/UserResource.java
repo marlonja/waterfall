c1b47a90ee66de41fdda6679e0318f04cfaad1e0
Lena <lena.stridsberg@gmail.com>
2016-09-20 13:39:18 +0200

    start using rest

---
+++ src/com/waterfall/RESTResources/UserResource.java
@@ -0,0 +1,17 @@
+package com.waterfall.RESTResources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/users")
+public class UserResource {
+	
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getUsers() {
+		return "Nu har vi hämtat alla användare";
+	}
+
+}
