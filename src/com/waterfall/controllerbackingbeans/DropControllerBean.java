1db7d8d5603568c031f04a2704120d6890cbcf58
Joel Nilsson <joel_send_now@hotmail.com>
2016-09-28 14:35:43 +0200

    Integrate maven in project

--- src/com/waterfall/controllerbackingbeans/DropControllerBean.java
+++ src/com/waterfall/controllerbackingbeans/DropControllerBean.java
@@ -4,12 +4,15 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 
+import com.waterfall.EJB.interfaces.LocalComment;
 import com.waterfall.EJB.interfaces.LocalDrop;
 import com.waterfall.EJB.interfaces.LocalUser;
+import com.waterfall.models.CommentModel;
 import com.waterfall.models.DropModel;
 
 import jersey.repackaged.com.google.common.collect.Lists;
@@ -21,6 +24,7 @@ public class DropControllerBean implements Serializable{
 	private static final long serialVersionUID = 2772076160829404613L;
 	
 	private String content;
+	private String commentContent;
 	private List<DropModel> dropList;
 	
 	@EJB
@@ -28,9 +32,13 @@ public class DropControllerBean implements Serializable{
 	
 	@EJB
 	LocalDrop dropEJB;
-
-	public List<DropModel> getAllDrops(){
-		return (List<DropModel>) Lists.reverse(dropEJB.getAllDrops());
+	
+	@EJB
+	LocalComment commentEJB;
+	
+	@PostConstruct
+	public void init() {
+		dropList = Lists.reverse(dropEJB.getAllDrops());
 		
 	}
 
@@ -45,6 +53,22 @@ public class DropControllerBean implements Serializable{
 		System.out.println("skapar droppe");
 		content = null;
 		return "index";
+	}
+	
+	public String createNewComment(Long dropId) {
+		CommentModel commentModel = new CommentModel();
+		System.out.println(commentContent);
+		System.out.println(userEJB.getUserFromSession("loggedInUser"));
+		System.out.println("user id: " + userEJB.getUserFromSession("loggedInUser").getUserid());
+		System.out.println("drop id:" + dropId);
+		commentModel.setContent(commentContent);
+		commentModel.setDropHost(dropEJB.getDrop(dropId));
+		commentModel.setOwner(userEJB.getUserFromSession("loggedInUser"));
+		commentEJB.storeComment(commentModel);
+		dropList = Lists.reverse(dropEJB.getAllDrops());
+		commentContent = null;
+		return "/index.xhtml?faces-redirect=true";
+		
 	}
 
 	public String getContent() {
@@ -62,6 +86,14 @@ public class DropControllerBean implements Serializable{
 	
 	public void setDropList(List<DropModel> dropList) {
 		this.dropList = dropList;
+	}
+
+	public String getCommentContent() {
+		return commentContent;
+	}
+
+	public void setCommentContent(String commentContent) {
+		this.commentContent = commentContent;
 	}
 	
 	
