3a5c5c9e9916cab04ebd203bf3d74bb960de81e1
Joel Nilsson <joel_send_now@hotmail.com>
2016-10-26 16:05:30 +0200

    start crud soap

--- src/com/waterfall/RESTResources/UserSoapResource.java
+++ src/com/waterfall/RESTResources/UserSoapResource.java
@@ -1,5 +1,8 @@
 package com.waterfall.RESTResources;
 
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.spec.InvalidKeySpecException;
 import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
@@ -8,7 +11,10 @@ import javax.ejb.EJB;
 import javax.enterprise.inject.New;
 import javax.jws.WebService;
 
+import org.eclipse.persistence.jpa.jpql.parser.ElseExpressionBNF;
+
 import com.waterfall.EJB.interfaces.LocalUser;
+import com.waterfall.hashing.pbkdf2.PBKDF2;
 import com.waterfall.models.DropModel;
 import com.waterfall.models.UserModel;
 import com.waterfall.serviceEJB.UserServiceEJB;
@@ -22,28 +28,41 @@ public class UserSoapResource {
 	public UserModel createUser(String birthdateFromUser, String city, 
 						String country, String email, String firstName,
 						String gender, String lastName, String pwFromUser,
-						String username) {
+						String username) throws NoSuchAlgorithmException, NoSuchProviderException, InvalidKeySpecException {
 		UserModel userModel = new UserModel();
-		Date birthdate = new Date(0, 0, 0);
 		
-		//TODO FINISH THIS
 		
+		String[] birthdateInfo = birthdateFromUser.split(",");
+		int birthyear = Integer.parseInt(birthdateInfo[0]);
+		int birthMonth = Integer.parseInt(birthdateInfo[1]);
+		int birthDay = Integer.parseInt(birthdateInfo[2]);
+		Date birthdate = new Date(birthyear -1900, birthMonth-1, birthDay);
+	
 		userModel.setBirthdate(birthdate);
 		userModel.setCity(city);
 		userModel.setCountry(country);
-		userModel.setDrops(new ArrayList<>());
 		userModel.setEmail(email);
 		userModel.setFirstName(firstName);
 		userModel.setGender(gender);
 		userModel.setLastName(lastName);
-		userModel.setPassword(pwFromUser);
+		userModel.setPassword(PBKDF2.generatePasswordHash(pwFromUser));
 		userModel.setUsername(username);
 		
-		return userModel;
+		if(userEjb.storeUser(userModel)) {
+			System.out.println("sparat");
+			return userModel;
+		}else {
+			System.out.println("INTE sparat");
+			return null;
+		}
 	}
 	
 	public UserModel getUser(Long userId) {
 		return userEjb.getUser(userId);
+	}
+	
+	public UserModel updateUser() {
+		return null;
 	}
 	
 	public List<UserModel> getAllUsers() {
