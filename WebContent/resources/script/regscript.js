40caa6ea80d6c8b017fb7a91b9005b038cc17904
apget <linisa89@hotmail.com>
2016-11-04 14:30:31 +0100

    fix registration validation

--- WebContent/resources/script/regscript.js
+++ WebContent/resources/script/regscript.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 		var errorsJsonFormat = $("#error-msg").html();
 		var errorMessages = JSON.parse(errorsJsonFormat);
-		var errorStyle = "input-error";
+		var errorStyle = "error";
 		console.log(errorMessages);
 		
 		for(var i = 0; i < errorMessages.length; i++){
